bd5e9705c9577c0504ecb96a27f50a36f082ab79
flintify <retardlandnga@gmail.com>
2026-06-19 07:45:10 +0300

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,16 @@
+local data = {}
+data.version = function()
+  return "1.8.5"
+end
+
+data.key = function()
+  return "flint" -- Lenght 8
+end
+
+data.info = function()
+  return {
+    title = "ShinyTool Info",
+    description = "Visit our discord server to get key."
+  }
+end
+return data
